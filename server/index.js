d4d707e2b2cd2fa0797515329dd48d10230550a5
Abhi <abhinavgupta12006@gmail.com>
2026-04-26 01:55:21 +0530

    server commit

--- server/index.js
+++ server/index.js
@@ -18,6 +18,10 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 
+const path = require('path');
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
 const roomManager = new RoomManager();
 
 // REST endpoints
@@ -50,6 +54,12 @@ setupSocketHandlers(io, roomManager);
 setInterval(() => {
   roomManager.cleanupStaleRooms();
 }, 5 * 60 * 1000);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
