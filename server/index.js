7e5b942bd6ba082d1786f850ba0c373a194d18c7
Abhi <abhinavgupta12006@gmail.com>
2026-04-26 18:34:20 +0530

    Fix Express 5 wildcard routing error

--- server/index.js
+++ server/index.js
@@ -57,8 +57,12 @@ setInterval(() => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+app.use((req, res, next) => {
+  if (req.method === 'GET' && req.accepts('html')) {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  } else {
+    next();
+  }
 });
 
 const PORT = process.env.PORT || 3001;
